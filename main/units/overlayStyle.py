7be478ac24463d2a4f0eba877d742cb41ce949a0
panggin <hbkim14@naver.com>
2024-04-04 20:27:12 +0900

    글자 크기 변경

--- main/units/overlayStyle.py
+++ main/units/overlayStyle.py
@@ -1,9 +1,9 @@
 default_style = '''background-color: rgba(255, 255, 255, 100); 
                     color: White; 
                     font-family: Arial; 
-                    font-size: 30px;'''
+                    font-size: 15px;'''
 
 transparent_style = '''background-color: rgba(255, 255, 255, 50); 
                     color: transparent;
                     font-family: Arial; 
-                    font-size: 30px;'''
+                    font-size: 15px;'''
