6a5fb9dd35be53f7627a04d8b0b87b9dd7e74503
panggin <hbkim14@naver.com>
2024-05-27 19:51:05 +0900

    패키지 __init__.py 파일 추가

---
+++ main/liveTranslator/capture/__init__.py
@@ -0,0 +1,4 @@
+# liveTranslator/capture/__init__.py
+
+from .wincapHandler import WindowCaptureHandler
+from .debug_wincapHandler import WindowCaptureHandlerForDebug
---
+++ main/liveTranslator/overlay/__init__.py
@@ -0,0 +1,8 @@
+# liveTranslator/overlay/__init__.py
+
+from .overlayStyle import StyleSheet
+
+from .overlayRangeWin import OverlayRangeWin
+from .overlayTextWin import OverlayTextWin
+
+from .controlPanel import ControlPanel
