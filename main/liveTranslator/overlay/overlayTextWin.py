8f118ca58a8d9b977d0110a508138a3559bf880b
panggin <hbkim14@naver.com>
2024-05-28 22:59:08 +0900

    오버레이 텍스트 배치 창 구현 및 테스트

---
+++ main/liveTranslator/overlay/overlayTextWin.py
@@ -0,0 +1,89 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QObject, Qt, QEvent, QRect, pyqtSignal, pyqtSlot
+
+from .overlayRangeWin import OverlayRangeWin
+from .overlayStyle import *
+
+
+class OverlayTextWin(OverlayRangeWin):
+
+    fKeyPressedToCap = pyqtSignal(QRect)  # F키가 눌렸음을 화면 캡처 스레드에 알리는 시그널
+    fKeyPressedToCtrl = pyqtSignal()      # F키가 눌렸음을 컨트롤 패널에 알리는 시그널
+
+    def __init__(self, limitRect=None):
+        super().__init__() # OverlayRangeWin.__init__() 호출
+        self.initFlag()
+
+        self.limit_rect = limitRect if limitRect is not None else self.limit_rect
+        self.setLabelGeometryWithGlobalRect(self.limit_rect)
+
+        print('>>> init OverlayTextWindow')
+
+    def initFlag(self):
+        super().initFlag()
+        self.lock_resize = False # 크기 고정 여부
+
+    #-------------------------------------------------------------
+
+    # 마우스가 라벨에 올라갔을 때 이벤트 처리 -> 투명화
+    def enterEvent(self, event):
+        if self.lock_resize:
+            self.label.setStyleSheet(StyleSheet.transparent_style)
+
+    # 마우스가 라벨에서 벗어났을 때 이벤트 처리
+    def leaveEvent(self, event):
+        if self.lock_resize:
+            self.label.setStyleSheet(StyleSheet.default_style)
+
+
+    # 이벤트 필터
+    def eventFilter(self, obj:QObject, event:QEvent):
+        # R키 누름 - 창 크기 재조정
+        if not self.lock_resize and event.type() == QEvent.KeyPress and event.key() == Qt.Key_R:
+            print('>>> press key R - reset overlay range')
+            self.initFlag() # 플래그 초기화
+            self.setLabelGeometryWithGlobalRect(self.limit_rect)
+            return True
+
+        # F키 누름 - 창 크기 고정
+        if not self.lock_resize and event.type() == QEvent.KeyPress and event.key() == Qt.Key_F:
+            print('>>> press key F - lock window size')
+            self.lock_resize = True
+            self.enable_drawing = False
+
+            self.label.setStyleSheet(StyleSheet.default_style)
+
+            self.fKeyPressedToCap.emit(self.label_rect)  # 화면 캡처 스레드로 선택 범위 전달
+            self.fKeyPressedToCtrl.emit()                # 컨트롤 패널로 시그널 전달
+            return True
+
+        # Q키 누름 - 프로그램 종료
+        if event.type() == QEvent.KeyPress and event.key() == Qt.Key_Q:
+            print('>>> press key Q - application exit')
+            QApplication.quit()
+            return True
+        
+        return super().eventFilter(obj, event)
+
+    #-------------------------------------------------------------
+
+    def setLimitRect(self, limitRect:QRect):
+        self.limit_rect = limitRect
+        self.setLabelGeometryWithGlobalRect(self.limit_rect)
+
+    def getLabelRect(self):
+        labelRect = self.label_rect if self.lock_resize else None
+        return labelRect
+    
+    @pyqtSlot(str) # OverlayTextWin <-- WindowCaptureHandler
+    def updateLabelText(self, text:str):
+        if text is not None:
+            self.label.setText(text)
+
+    @pyqtSlot()
+    def updateOverlayStyle(self):
+        self.label.setStyleSheet(StyleSheet.default_style)
+    
+    
---
+++ test/overlayTextWinTest.py
@@ -0,0 +1,94 @@
+# test/overlayTextWinTest.py <- liveTranslator/overlay/overlayTextWin.py
+
+import sys
+import time
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt, QObject, QThread, QTimer, QRect, QEvent, pyqtSignal, pyqtSlot
+
+from path_liveTranslator import *
+from main.liveTranslator.overlay import OverlayTextWin
+
+
+# 시그널 전달을 확인하기 위한 스레드
+class SignalEmit(QThread):
+    transText = pyqtSignal(str)      # 오버레이 창에 배치될 텍스트 전달 시그널
+    styleSheetUpdated = pyqtSignal() # 오버레이 스타일시트 변경을 알리는 시그널
+
+    def __init__(self):
+        super().__init__()
+        self.transTextList = ['Hello', 'World', 'Python', 'Overlay', 'Text', 'Window']
+
+    def run(self):
+        self.emitTransText()
+        self.emitStyleSheetUpdated()
+
+    @pyqtSlot(QRect)
+    def fKeySignalToCapReceiver(self, wincap_rect:QRect):
+        print(f'F key signal to capture window received : {wincap_rect}')
+        self.start()
+
+    @pyqtSlot()
+    def fKeySignalToCtrlReceiver(self):
+        print('F key signal to control panel received')
+
+    def emitTransText(self):
+        for text in self.transTextList:
+            print(f'Translated text emit : {text}')
+            self.transText.emit(text)
+            time.sleep(2)
+
+    def emitStyleSheetUpdated(self):
+        self.styleSheetUpdated.emit()
+        
+#-------------------------------------------------------------
+
+# OverlayTextWin의 디버깅 클래스
+class OverlayTextWinForDebug(OverlayTextWin):
+
+    # 이벤트 필터 확인
+    def eventFilter(self, obj:QObject, event:QEvent):
+
+        # F키 누름 - 창 크기 고정
+        if not self.lock_resize and event.type() == QEvent.KeyPress and event.key() == Qt.Key_F:
+            print('Key F signal emit')
+            print(f'label_rect : {self.label_rect}')
+
+        # Q키 누름 - 프로그램 종료
+        if event.type() == QEvent.KeyPress and event.key() == Qt.Key_Q:
+            print('Application exit')
+        
+        return super().eventFilter(obj, event)
+    
+    # 시그널 전달받음 확인
+    @pyqtSlot(str) # OverlayTextWin <-- WindowCaptureHandler
+    def updateLabelText(self, text:str):
+        print(f'translated text signal received : {text}')
+        return super().updateLabelText(text)
+
+    @pyqtSlot()
+    def updateOverlayStyle(self):
+        print(f'stylesheet updated signal received')
+        return super().updateOverlayStyle()
+
+
+
+if __name__ == '__main__':
+    print(sys.path)
+    
+    app = QApplication(sys.argv)
+
+    limitRect = QRect(310, 78, 1199, 626) # 클래스 생성 시 사용
+    signalEmit = SignalEmit() # 텍스트 배치 확인을 위한 시그널 발생 클래스
+
+    overlay = OverlayTextWinForDebug(limitRect)
+    # overlay = OverlayTextWin(limitRect)
+    overlay.show()
+
+    # F키 시그널 전달 확인
+    overlay.fKeyPressedToCap.connect(signalEmit.fKeySignalToCapReceiver)
+    overlay.fKeyPressedToCtrl.connect(signalEmit.fKeySignalToCtrlReceiver)
+
+    signalEmit.transText.connect(overlay.updateLabelText)
+    signalEmit.styleSheetUpdated.connect(overlay.updateOverlayStyle)
+
+    sys.exit(app.exec_())
